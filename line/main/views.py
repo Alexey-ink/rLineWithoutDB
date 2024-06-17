2ef235aa385f47f93e6dd0ca97dce3b4406f76b5
Alexey Shihalev <alexey.shihalev@internet.ru>
2024-06-17 21:35:42 +0300

    added saving requests

--- line/main/views.py
+++ line/main/views.py
@@ -4,11 +4,16 @@ import os
 from django.http import HttpResponse
 from moviepy.editor import TextClip, CompositeVideoClip, ColorClip
 from moviepy.config import change_settings
+from .models import UserRequest
 
 # указываем путь к исполняемому файлу convert.exe
 change_settings({"IMAGEMAGICK_BINARY": "E:\\Programs\\ImageMagick-7.1.1-Q16-HDRI\\magick.exe"})
 
 def generate_video(request, text):
+
+    # сохраняем запрос в базу данных
+    UserRequest.objects.create(text=text)
+
     video_width, video_height = 100, 100
     duration = 3  # продолжительность видео в секундах
     fps = 24  # кадров в секунду
